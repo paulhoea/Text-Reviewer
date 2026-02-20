ed78a91a14299fa7f4bef1b20c704c3c1707c61d
Paul H. <thepaulhoefle@gmail.com>
2026-02-20 13:28:27 +0100

    Prepare http request and HTML parsing

--- scrape.py
+++ scrape.py
@@ -2,6 +2,8 @@
 import requests
 import pandas as pd
 import xmltodict
+from bs4 import BeautifulSoup
+import re
 
 # paths
 # path_input = "/home/paul/Documents/TextReviewer/Input/sitemap-posts.xml"
@@ -18,9 +20,9 @@ print("Number of URLs:", len(inputlinks))
 inputlinks = pd.DataFrame(inputlinks, columns=["links", "lastmod"])
 inputlinks["lastmod"] = pd.to_datetime(inputlinks["lastmod"])
 
-# %% filter only album reviews
-inputlinks["relevant"] = inputlinks["links"].str.contains("album-reviews")
-inputlinks = inputlinks[inputlinks["relevant"] == True]
+# %% filter only album reviews, and remove staff reviews
+inputlinks = inputlinks[inputlinks["links"].str.contains("album-reviews")]
+inputlinks = inputlinks[~inputlinks["links"].str.contains("tnd-staff")]
 
 # filter to date where transcripts are availible
 # NOT NESSECARY AS THE NAMING FORMAT WITH "album-review" in the URL was only adopted here, but kept for completeness
@@ -28,4 +30,26 @@ inputlinks = inputlinks[inputlinks["lastmod"] > "2024-07-31"]
 
 # reset index for further processing
 inputlinks.reset_index(drop = True, inplace = True)
+
+
+# %% Test request and HTML parsing
+x = inputlinks[inputlinks.index == 10]
+
+for index, row in x.iterrows():
+    print(row["links"])
+    page = requests.get(row["links"])
+    parsed_html = BeautifulSoup(page.text)
+
+    # <meta content="9/10" property="article:tag"/>; question if this is universal. One approach would be to extract these via regex
+    rating = None
+    for tag in parsed_html.find_all("meta", property="article:tag"):
+        content = tag.get("content", "")
+        if re.fullmatch(r"\d+/10", content):
+            rating = content
+            break
+    x.loc[index, "rating"] = rating
+    
+
+
+
 # %%
